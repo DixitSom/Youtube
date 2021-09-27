172aa97ebe14a44e8a3c49713d33e274fc4e478b
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-09-27 15:22:47 +0530

    URLS updated

--- Youtube/main/urls.py
+++ Youtube/main/urls.py
@@ -1,10 +1,13 @@
+from os import name
 from django.contrib import admin
 from django.urls import path
 
-from main.views import index, login_view, signUp
+from main.views import index, login_view, signUp, createChannel, channel_view
 
 urlpatterns = [
     path('', index, name='home'),
     path('login/', login_view, name='login'),
-    path('register/', signUp, name='register')
+    path('register/', signUp, name='register'),
+    path('createChannel/', createChannel, name='createChannel'),
+    path('channel/', channel_view, name='channel')
 ]
