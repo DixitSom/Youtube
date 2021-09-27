0914ee89ae7022568217dec5ff9ab6635dfd19d1
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-09-27 15:52:38 +0530

    All Models registered

--- Youtube/main/admin.py
+++ Youtube/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Content)
+admin.site.register(Comment)
+admin.site.register(Interaction)
+admin.site.register(Channel)
+
+
