27f40c1727a1e901df1cd5747f85c390729b8d79
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-09-27 10:24:41 +0530

    Updated Path

--- Youtube/main/urls.py
+++ Youtube/main/urls.py
@@ -1,10 +1,10 @@
-from main.views import login_view
 from django.contrib import admin
 from django.urls import path
 
-from main.views import index
+from main.views import index, login_view, signUp
 
 urlpatterns = [
     path('', index, name='home'),
-    path('login/', login_view, name='login')
+    path('login/', login_view, name='login'),
+    path('register/', signUp, name='register')
 ]
