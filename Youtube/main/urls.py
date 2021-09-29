22b441e42582b4913fb03dd45fd14cb15aef8d06
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-09-29 11:25:00 +0530

    URL added for single Video Page

--- Youtube/main/urls.py
+++ Youtube/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from main.views import index, login_view, signUp, createChannel, channel_view, signout, uploadContent
+from main.views import index, login_view, signUp, createChannel, channel_view, signout, uploadContent, video
 
 urlpatterns = [
     path('', index, name='home'),
@@ -11,5 +11,6 @@ urlpatterns = [
     path('register/', signUp, name='register'),
     path('createChannel/', createChannel, name='createChannel'),
     path('channel/', channel_view, name='channel'),
-    path('upload/', uploadContent, name='upload')
+    path('upload/', uploadContent, name='upload'),
+    path('video/<int:video_id>/', video, name='video')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
