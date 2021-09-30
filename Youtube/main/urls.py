d0ee9e28beed4bacdbd2fe27e0f2c44c48699551
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-09-30 12:38:29 +0530

    URL added for interactio store and status

--- Youtube/main/urls.py
+++ Youtube/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from main.views import index, login_view, signUp, createChannel, channel_view, signout, uploadContent, video
+from main.views import index, login_view, signUp, createChannel, channel_view, signout, uploadContent, video, interaction_status, feed_interaction
 
 urlpatterns = [
     path('', index, name='home'),
@@ -12,5 +12,7 @@ urlpatterns = [
     path('createChannel/', createChannel, name='createChannel'),
     path('channel/', channel_view, name='channel'),
     path('upload/', uploadContent, name='upload'),
-    path('video/<int:video_id>/', video, name='video')
+    path('video/<int:video_id>/', video, name='video'),
+    path('interaction-status/', interaction_status, name='interaction_status'),
+    path('feed-interaction/', feed_interaction, name='feed_interaction')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
